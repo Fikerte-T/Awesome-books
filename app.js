617588ec010d9ceec880e438680c4d233eec27d1
mahbub <mahbub21alam2017@gmail.com>
2021-09-14 14:20:21 +0600

    Update js file

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function saveBooks(){
     localStorage.setItem('books', myBooks)
 }
 function display(){
-    displayBooks.innerHTML = ' ';
+    displayBooks.innerHTML = '';
     books.forEach((book) => {
         const bookContainer = document.createElement('div');
         const bookTitle = document.createElement('p');
@@ -56,53 +56,5 @@ function display(){
       }
   window.onload = checkStorage;
   display();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const books = [
-//     {
-//         title:  'Lorum ipsum',
-//         author: 'Testeroo Testyy'
-//     },
-//     {
-//         title: 'Second book',
-//         author: 'Testeroo Testyy'
-//     }
-// ]
-
-// function addBook(){
-//     title.value= '';
-//     author.value= '';
-//     addBook(bookTitle, bookAuthor) {
-//         if(bookTitle.value != '' && bookAuthor.value != ''){
-//             let li1 = document.createElement('li');
-//             li1.innerText = bookTitle.value;
-//             list.appendChild(li1);
-//             this.title = bookTitle.value;
-//             let li2 = document.createElement('li');
-//             li2.innerText = bookAuthor.value;
-//             list.appendChild(li2);
-//             this.author = bookAuthor.value
-//             const btnRemove = document.createElement('button');
-//             btnRemove.innerText = 'Remove';
-//             list.appendChild(btnRemove);
-//             const horizontal = document.createElement('hr');
-//             list.appendChild(horizontal);
-//         }
-//     },
-// }
-// addButton.addEventListener('click', addBook());
 
 
