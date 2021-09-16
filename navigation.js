bd9c429d52b8cc25a7834a914091cd2d0651826a
mahbub <mahbub21alam2017@gmail.com>
2021-09-16 19:09:18 +0600

    Add navigation

--- navigation.js
+++ navigation.js
@@ -1,15 +1,31 @@
+// const { userInfo } = require("os");
+
 const bookList = document.querySelector('.book-list');
 const formContainer = document.querySelector('.form-container');
 const contact = document.querySelector('.contact');
 const list = document.querySelector('#list');
 const addNew = document.querySelector('#add-new');
-const contactUs = document.querySelector('#contact');
+const contactUs = document.getElementById('contact');
 
-list.addEventListener('click', () => {
-    formContainer.classList.add('remove');
-    contact.classList.add('remove');
-    // document.getElementsByClassName("form-container").style.visibility = "hidden";
+list.addEventListener('click', function(){
+    document.querySelector('.book-list').style.display = 'block';
+    document.querySelector(".form-container").style.display = "none";
+    document.querySelector(".contact").style.display = "none";
 });
+
+addNew.addEventListener('click', function(){
+    document.querySelector(".book-list").style.display = "none";
+    document.querySelector('.form-container').style.display = 'block';
+    document.querySelector(".contact").style.display = "none";
+});
+
+contactUs.addEventListener('click', function(){
+    document.querySelector(".form-container").style.display = "none";
+    document.querySelector(".book-list").style.display = "none";
+    document.querySelector('.contact').style.display = 'block';
+});
+
+
 
 
 
