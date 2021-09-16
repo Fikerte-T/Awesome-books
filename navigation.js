12286bc09e1b340de94c1408ad1e8ba90ec9a994
mahbub <mahbub21alam2017@gmail.com>
2021-09-16 22:49:02 +0600

    Fix linters error

--- navigation.js
+++ navigation.js
@@ -1,31 +1,30 @@
-const bookList = document.querySelector('.book-list');
 const formContainer = document.querySelector('.form-container');
 const contact = document.querySelector('.contact');
 const list = document.querySelector('#list');
 const addNew = document.querySelector('#add-new');
 const contactUs = document.getElementById('contact');
-const hideList = document.querySelector('#displayBooks')
 
-formContainer.style.display='none';
+formContainer.style.display = 'none';
 contact.style.display = 'none';
 
-
-list.addEventListener('click', function(){
-    document.querySelector('.book-list').style.display = 'block';
-    document.querySelector(".form-container").style.display = "none";
-    document.querySelector(".contact").style.display = "none";
+list.addEventListener('click', () => {
+  document.querySelector('.book-list').style.display = 'block';
+  document.querySelector('.form-container').style.display = 'none';
+  document.querySelector('.contact').style.display = 'none';
 });
 
-addNew.addEventListener('click', function(){
-    document.querySelector(".book-list").style.display = "none";
-    document.querySelector('.form-container').style.display = 'block';
-    document.querySelector(".contact").style.display = "none";
+addNew.addEventListener('click', () => {
+  document.querySelector('.book-list').style.display = 'none';
+  document.querySelector('.form-container').style.display = 'block';
+  document.querySelector('.contact').style.display = 'none';
 });
 
-contactUs.addEventListener('click', function(){
-    document.querySelector(".form-container").style.display = "none";
-    document.querySelector(".book-list").style.display = "none";
-    document.querySelector('.contact').style.display = 'block';
+contactUs.addEventListener('click', () => {
+  document.querySelector('.form-container').style.display = 'none';
+  document.querySelector('.book-list').style.display = 'none';
+  document.querySelector('.contact').style.display = 'block';
 });
 
-let DateTime = luxon.DateTime;
+const date = document.getElementById('date');
+// eslint-disable-next-line no-undef
+date.innerText = luxon.DateTime.now().toFormat('MMMM dd, yyyy, hh:mm:ss a');
